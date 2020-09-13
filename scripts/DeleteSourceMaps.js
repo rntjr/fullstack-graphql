ae596cb78ba693c5deae96e45aaeab8c53d33810
Renato Junior <48002341+rntjr@users.noreply.github.com>
2020-09-13 15:54:40 -0300

    chore: removido scripts do projeto

--- scripts/DeleteSourceMaps.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable space-before-function-paren */
-const path = require('path')
-const rimraf = require('rimraf')
-
-export default function deleteSourceMaps() {
-  rimraf.sync(path.join(__dirname, '../../app/dist/*.js.map'))
-  rimraf.sync(path.join(__dirname, '../../app/*.js.map'))
-}
